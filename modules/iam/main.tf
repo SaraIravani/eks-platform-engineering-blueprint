5bfc7dd3f19854478fc8284218100c4f74eba4d4
Sara Iravani <sarairavani@outlook.com>
2026-05-07 23:43:17 +0000

    feat(iam): attach node group managed policies

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -36,4 +36,17 @@ resource "aws_iam_role" "eks_node_group" {
   name               = "${var.cluster_name}-node-group-role"
   assume_role_policy = data.aws_iam_policy_document.eks_node_assume_role.json
 }
+resource "aws_iam_role_policy_attachment" "worker_node_policy" {
+  role       = aws_iam_role.eks_node_group.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
 
+resource "aws_iam_role_policy_attachment" "ecr_read_only" {
+  role       = aws_iam_role.eks_node_group.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
+
+resource "aws_iam_role_policy_attachment" "cni_policy" {
+  role       = aws_iam_role.eks_node_group.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+}
