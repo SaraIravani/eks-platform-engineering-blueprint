4bd746a8927a23587f430e0f781187882f44ef96
Sara Iravani <sarairavani@outlook.com>
2026-05-07 23:42:00 +0000

    feat(iam): create eks node group iam role

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -19,3 +19,21 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_policy" {
   role       = aws_iam_role.eks_cluster.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
 }
+data "aws_iam_policy_document" "eks_node_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "eks_node_group" {
+  name               = "${var.cluster_name}-node-group-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_node_assume_role.json
+}
+
