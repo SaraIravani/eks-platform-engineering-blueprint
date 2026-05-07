b8fbccc96db7e4d3a721948856e74b68a737e11e
Sara Iravani <sarairavani@outlook.com>
2026-05-07 23:36:52 +0000

    feat(iam): create iam module skeleton,add iam module variables

---
+++ modules/iam/main.tf
@@ -0,0 +1,17 @@
+data "aws_iam_policy_document" "eks_cluster_assume_role" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["eks.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+resource "aws_iam_role" "eks_cluster" {
+  name               = "${var.cluster_name}-cluster-role"
+  assume_role_policy = data.aws_iam_policy_document.eks_cluster_assume_role.json
+}
