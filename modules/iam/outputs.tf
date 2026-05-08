5267dfc64d7daf24ce4aed3ace91fd23582fcfaa
Sara Iravani <sarairavani@outlook.com>
2026-05-08 22:38:35 +0000

    feat(iam): output ebs csi role arn

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -5,3 +5,7 @@ output "eks_cluster_role_arn" {
 output "eks_node_group_role_arn" {
   value = aws_iam_role.eks_node_group.arn
 }
+output "ebs_csi_role_arn" {
+  description = "IAM role ARN for EBS CSI driver IRSA"
+  value       = try(aws_iam_role.ebs_csi[0].arn, null)
+}
