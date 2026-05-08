40fb5e760f4b994db095cd4bdced433fa23feadf
Sara Iravani <sarairavani@outlook.com>
2026-05-08 22:26:19 +0000

    feat(addons): install coredns addon

--- modules/addons/main.tf
+++ modules/addons/main.tf
@@ -4,3 +4,9 @@ resource "aws_eks_addon" "vpc_cni" {
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 }
+resource "aws_eks_addon" "coredns" {
+  cluster_name                = var.cluster_name
+  addon_name                  = "coredns"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+}
