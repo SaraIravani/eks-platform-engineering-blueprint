1b3c8e6565e63044b3fe213a17e929569c11429c
Sara Iravani <sarairavani@outlook.com>
2026-05-08 23:30:12 +0000

    feat(addons): install ebs csi driver addon

--- modules/addons/main.tf
+++ modules/addons/main.tf
@@ -16,3 +16,10 @@ resource "aws_eks_addon" "kube_proxy" {
   resolve_conflicts_on_create = "OVERWRITE"
   resolve_conflicts_on_update = "OVERWRITE"
 }
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name                = var.cluster_name
+  addon_name                  = "aws-ebs-csi-driver"
+  service_account_role_arn    = var.ebs_csi_role_arn
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+}
