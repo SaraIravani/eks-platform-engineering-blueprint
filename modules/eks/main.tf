29402f6cf1cb4fd91b3f50da960d28d1d7e1215c
Sara Iravani <sarairavani@outlook.com>
2026-05-08 02:42:38 +0000

    feat(eks): create eks control plane

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -12,3 +12,32 @@ resource "aws_kms_alias" "eks" {
   name          = "alias/${var.cluster_name}-eks"
   target_key_id = aws_kms_key.eks.key_id
 }
+resource "aws_eks_cluster" "this" {
+  name     = var.cluster_name
+  version  = var.cluster_version
+  role_arn = var.cluster_role_arn
+
+  vpc_config {
+    subnet_ids              = var.subnet_ids
+    endpoint_private_access = var.endpoint_private_access
+    endpoint_public_access  = var.endpoint_public_access
+  }
+
+  enabled_cluster_log_types = var.enabled_cluster_log_types
+
+  encryption_config {
+    provider {
+      key_arn = aws_kms_key.eks.arn
+    }
+
+    resources = ["secrets"]
+  }
+
+  depends_on = [
+    aws_kms_key.eks
+  ]
+
+  tags = {
+    Name = var.cluster_name
+  }
+}
