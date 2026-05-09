5b90ab11f9647a60fb4f3c3e96c4b8f912faf8c8
Sara Iravani <sarairavani@outlook.com>
2026-05-09 17:16:52 +0000

    feat(storage): add high performance io2 storage class

--- modules/storage/main.tf
+++ modules/storage/main.tf
@@ -17,3 +17,19 @@ resource "kubernetes_storage_class" "gp3" {
     encrypted = "true"
   }
 }
+resource "kubernetes_storage_class" "io2" {
+  metadata {
+    name = "io2"
+  }
+
+  storage_provisioner    = "ebs.csi.aws.com"
+  reclaim_policy         = "Retain"
+  volume_binding_mode    = "WaitForFirstConsumer"
+  allow_volume_expansion = true
+
+  parameters = {
+    type      = "io2"
+    encrypted = "true"
+    iops      = "3000"
+  }
+}
